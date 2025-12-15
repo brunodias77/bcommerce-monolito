21ec3d3d0e0245483e2c301d3048413aac7d4d16
brunodias77 <brunohdias95@gmail.com>
2025-12-14 22:29:26 -0300

    ...

--- src/api/Bcommerce.Api/Program.cs
+++ src/api/Bcommerce.Api/Program.cs
@@ -1,7 +1,12 @@
 using Bcommerce.Api.Configurations;
+using Serilog;
 
 // Cria o WebApplicationBuilder, que inicializa configurações, logging e DI container.
 var builder = WebApplication.CreateBuilder(args);
+
+// Configuração do Serilog
+builder.Host.UseSerilog((context, config) =>
+    config.ReadFrom.Configuration(context.Configuration));
 
 // ===== CONFIGURAÇÃO DE SERVIÇOS (Dependency Injection) =====
 // Aqui registramos todos os serviços, repositórios, contexto de banco, etc.
